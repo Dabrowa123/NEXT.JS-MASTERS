606948df58729946fa69c702e3e22e9b11b7730a
SDabrowa <dabrowa.szymon@gmail.com>
2024-03-11 17:59:52 +0100

    task-3 p1-7 fix

--- src/app/products/[page]/page.tsx
+++ src/app/products/[page]/page.tsx
@@ -1,43 +1,17 @@
 import { redirect } from "next/navigation";
 import { Pagination } from "@/ui/molecules/Pagination";
 import { ProductList } from "@/ui/organisms/ProductList";
-import { type ProductItemType } from "@/types";
-
-export async function generateStaticParams() {
-	let currentPage = 1;
-	const staticParams = [];
-
-	while (true) {
-		const response = await fetch(
-			`https://naszsklep-api.vercel.app/api/products?take=20&offset=${currentPage}`,
-		);
-		if (!response.ok) {
-			break;
-		}
-		const newProductsPage = (await response.json()) as ProductItemType[];
-		if (newProductsPage.length === 0) {
-			break;
-		}
-		if (currentPage === 5) {
-			break;
-		}
-		staticParams.push({ page: currentPage.toString() });
-
-		currentPage++;
-	}
-
-	return staticParams;
-}
+import { getProductsByPage, getProductsList } from "@/api/products";
 
 export default async function ProductsPage({ params }: { params: { page: number } }) {
-	const totalPages = (await generateStaticParams()).length;
-	const response = await fetch(
-		"https://naszsklep-api.vercel.app/api/products?take=20&offset=" + params.page,
-	);
-	const products = (await response.json()) as ProductItemType[];
+	const allProducts = await getProductsList();
+	const allProductsNumber = allProducts.length;
+	const productsPerPage = 4;
+	const totalPages = Math.ceil(allProductsNumber / productsPerPage);
+	const products = await getProductsByPage(params.page);
 
 	if (!params.page || isNaN(Number(params.page)) || Number(params.page) <= 0) {
-		return redirect("/products/1");
+		return redirect("/products");
 	}
 
 	return (
