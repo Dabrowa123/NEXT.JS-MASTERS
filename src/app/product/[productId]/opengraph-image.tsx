a48da999f783b02b4f648343a8f76af1bea86708
SDabrowa <dabrowa.szymon@gmail.com>
2024-03-15 18:17:23 +0100

    task-4 p1-2

---
+++ src/app/product/[productId]/opengraph-image.tsx
@@ -0,0 +1,40 @@
+import { notFound } from "next/navigation";
+import { ImageResponse } from "next/og";
+import { executeGraphql } from "@/api/graphqlApi";
+import { ProductGetByIdDocument } from "@/gql/graphql";
+
+export const runtime = "edge";
+
+export const alt = "About Acme";
+export const size = {
+	width: 1200,
+	height: 630,
+};
+export const contentType = "image/png";
+
+export async function Image({ params }: { params: { productId: string } }) {
+	const { product } = await executeGraphql(ProductGetByIdDocument, {
+		id: params.productId,
+	});
+	if (!product) {
+		notFound();
+	}
+	return new ImageResponse(
+		(
+			<div style={{ flexDirection: "row", alignItems: "center" }}>
+				<img
+					src={{ uri: product.images[0].url }}
+					style={{ width: 100, height: 100, marginRight: 10 }}
+				/>
+				<div>
+					<p style={{ fontSize: 18, fontWeight: "bold" }}>{product.name}</p>
+					<p style={{ fontSize: 16 }}>{product.description}</p>
+					<p style={{ fontSize: 16 }}>Category: {product.categories[0].name}</p>
+				</div>
+			</div>
+		),
+		{
+			...size,
+		},
+	);
+}
