4c7ae02ce8c22229ef551cc10949f88d49421767
SDabrowa <dabrowa.szymon@gmail.com>
2024-03-15 19:09:08 +0100

    task-4 p5

--- src/ui/molecules/ProductListItem.tsx
+++ src/ui/molecules/ProductListItem.tsx
@@ -10,16 +10,14 @@ type ProductListItemProps = {
 export const ProductListItem = ({ product }: ProductListItemProps) => {
 	return (
 		<li>
-			<a>
-				<Link href={`/product/${product.id}`}>
-					<article>
-						{product.images[0] && (
-							<ProductCoverImage src={product.images[0].url} alt={product.name} />
-						)}
-						<ProductListItemDescription product={product} />
-					</article>
-				</Link>
-			</a>
+			<Link href={`/product/${product.id}`}>
+				<article>
+					{product.images[0] && (
+						<ProductCoverImage src={product.images[0].url} alt={product.name} />
+					)}
+					<ProductListItemDescription product={product} />
+				</article>
+			</Link>
 		</li>
 	);
 };
